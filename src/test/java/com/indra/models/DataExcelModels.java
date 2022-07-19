ea727322d451db27463fff77e86ce9f37d368308
NataRiosA <nataliarios325@gmail.com>
2022-07-19 09:18:30 -0500

    Se crean nuevos casos para cesion de contrato nit a nit

--- src/test/java/com/indra/models/DataExcelModels.java
+++ src/test/java/com/indra/models/DataExcelModels.java
@@ -6,6 +6,11 @@ public class DataExcelModels{
 
     ReadFileXLSXActions fileXLSX = new ReadFileXLSXActions();
 
+    String clientNit;
+
+    String msisdnNintendoNit;
+    String msiNintendoNit;
+    String CedulaClienteNinNit;
     String urlEpos;
     String urlCRM;
     String urlComfirmador;
@@ -507,4 +512,28 @@ public class DataExcelModels{
         return passwordP;
     }
 
+
+    public String getMsisdnNintendoNit() {
+        fileXLSX.readFileExcel();
+        msisdnNintendoNit = fileXLSX.excelArray.get(15).get(2);
+        return msisdnNintendoNit;
+    }
+
+    public String getMsiNintendoNit() {
+        fileXLSX.readFileExcel();
+        msiNintendoNit = fileXLSX.excelArray.get(15).get(3);
+        return msiNintendoNit;
+    }
+
+    public String getCedulaClienteNinNit() {
+        fileXLSX.readFileExcel();
+        CedulaClienteNinNit = fileXLSX.excelArray.get(15).get(1);
+        return CedulaClienteNinNit;
+    }
+
+    public String getClientNit() {
+        fileXLSX.readFileExcel();
+        clientNit = fileXLSX.excelArray.get(12).get(4);
+        return clientNit;
+    }
 }
